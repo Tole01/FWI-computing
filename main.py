0eef346e746824011091873bd747bdcf9d035fae
JacoboT27 <jacobotc2703@gmail.com>
2025-04-28 13:25:42 -0600

    agregue funcion pixel_to_gps

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from coordinates import get_coordinates
 from coordinates import pixel_to_gps
 from risk_score import calculate_risk_score
 from deteccion_incendio import detect_fire
-from mesh_analysis import mesh_segmentation as seg
+#from mesh_analysis import mesh_segmentation as seg
 import cv2
 
 
 fire = 0 # Variable para indicar si hay fuego o no
-fire_img = detect_fire(fire) # Loop que busca fuego en las dos camaras, guarda la imagen optica con fuego
-seg(fire_img)
+fire_img,cx,cy = detect_fire(fire) # Loop que busca fuego en las dos camaras, guarda la imagen optica con fuego
+#seg(fire_img)
 # cv2.imshow("Imagen Óptica Capturada", fire_img)
 # Generar análisis de malla a partir de imagen
 
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
 # Coordenadas que se obtendran de ardupilot
-lat,lon = 25.618611, -100.356977 #get_coordinates()
+drone_lat,drone_lon,drone_height = 25.618611, -100.356977,35 #get_coordinates()
+# Coordenadas de la imagen transformadas a GPS
+lat, lon = pixel_to_gps(cx,cy,1920,1080,drone_height,drone_lat,drone_lon,160,90)
 
 #calorimetria
 T = 30 # Temperatura de la coordenada a evaluar
@@ -47,6 +50,9 @@ print("\n",ndvi)
 
 print("\n Slope:")
 print("\n",slope)
+
+print("\nlat: ", lat)
+print("\nlon: ", lon)
 
 #calcular riesgo de incendio
 risk = calculate_risk_score(indices, ndvi, slope, T_norm)
