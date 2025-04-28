566dcf2680d97b19ee971eccb6b6c317da05b6ea
JacoboT27 <jacobotc2703@gmail.com>
2025-04-28 09:54:57 -0600

    BUI normalizado

--- computingFWI.py
+++ computingFWI.py
@@ -157,7 +157,8 @@ class FWICLASS:
     if bui < 0.0:
       bui = 0.0
     # print('BUI:', bui)
-    return bui
+    normalized_bui = bui / 250  # Normalizar BUI a un rango de 0 a 1
+    return normalized_bui
 
   # Verificado
   def FWIcalc(self, isi, bui):
