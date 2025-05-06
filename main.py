2aff77b3255e798699822f6d8e2fd642b586953b
JacoboT27 <jacobotc2703@gmail.com>
2025-05-06 11:38:43 -0600

    Matriz de temperatura de ejemplo

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from risk_score import calculate_risk_score
 from deteccion_incendio import detect_fire
 import cv2
 
-
 fire = 0 # Variable para indicar si hay fuego o no
 fire_img,cx,cy = detect_fire(fire) # Loop que busca fuego en las dos camaras, guarda la imagen optica con fuego
 
@@ -25,17 +24,26 @@ lat, lon = pixel_to_gps(cx,cy,1920,1080,drone_height,drone_lat,drone_lon,157.1,1
 # Coordenadas del incendio -> Input para EQUIPO 2
 lat_fire, lon_fire = pixel_to_gps(cx,cy,1920,1080,drone_height,drone_lat,drone_lon,157.1,140.4)
 
+#obtener temperatura de cada coordenada (en pixel)
+temperature_matrix = [[100,80,40,40,80,60,30,30,30,95,90,100,100,25,25,25],
+                      [130,100,140,60,120,30,30,30,25,25,25,25,25,80,60,25],
+                      [100,120,35,100,80,80,30,30,90,30,80,25,25,25,25,25],
+                      [40,25,130,25,25,25,25,25,90,80,25,25,25,25,25,25],
+                      [100,25,25,130,25,25,100,25,25,25,95,80,25,25,25,25],
+                      [90,25,70,25,25,25,25,100,25,25,95,25,25,25,25,25],
+                      [25,110,25,25,60,25,25,25,25,25,25,100,90,25,25,25],
+                      [25,25,25,70,80,25,25,25,100,30,30,90,30,30,30,30],
+                      [30,30,30,30,30,30,30,30,30,30,30,30,30,70,30,30],             
+] # Matriz de temperaturas de ejemplo
+
+
+
+
 # Análisis de Mallado
 
 # Visualización del Análisis de Riesgo
 
 # Comunicación con la nube / Aplicación WEB
-
-
-
-#obtener temperatura de cada coordenada (en pixel)
-T = 30 # Temperatura de la coordenada a evaluar
-T_norm = (T+10)/(140+10) # Normalización de la temperatura
 
 # Obtener datos del clima
 row = get_weather_data(lat, lon)
