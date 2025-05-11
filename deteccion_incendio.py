16f4c1521417c3b50faca654f24e336abd8d97b9
JacoboT27 <jacobotc2703@gmail.com>
2025-05-11 14:48:15 -0600

    Se conceta a la camara optica

--- deteccion_incendio.py
+++ deteccion_incendio.py
@@ -11,29 +11,29 @@ def detect_fire(fire):
         fire (int): 1 si ya había fuego, 0 si se está buscando.
     
     Returns:
-        img_optica (numpy array): Imagen de la cámara en el momento de detección.
-        cx (int): Coordenada x del centroide del fuego.
-        cy (int): Coordenada y del centroide del fuego.
+        cx (int) Coordenada x del centroide del fuego.
+        cy (int) Coordenada y del centroide del fuego.
+        img_optica (numpy array) Imagen de la cámara en el momento de detección.
     """
-    model = YOLO(r"fire_s.pt")
-    '''cap = cv2.VideoCapture(0)
+    model = YOLO(r"fire_s.pt") #Modelo de deteccion entrenado
+    cap = cv2.VideoCapture(1) 
     if not cap.isOpened():
         print("No se pudo abrir la cámara.")
-        exit()'''
+        exit()
 
     img_optica = None  # Inicializamos variable
 
     while fire == 0:
-        '''ret, frame = cap.read()
+        ret, frame = cap.read()
         if not ret:
             print("No se pudo leer el frame de la cámara.")
-            break'''
+            break
 
         # Ejecutar inferencia
-        #results = model(frame, conf=0.3)[0]
-        results = model.predict(source=r"firetest11.jpg", conf=0.4)[0]
+        results = model(frame, conf=0.3)[0] #resultados de YOLO en el frame
+        results = model.predict(source=r"firetest11.jpg", conf=0.4)[0] #ejemplo se borra
         annotated_frame = results.plot()
-        frame = cv2.imread(r"firetest11.jpg")
+        frame = cv2.imread(r"firetest11.jpg") #ejemplo se borra
         fire_coordinates = []  # Lista para almacenar los pares (cx, cy)
         for box, cls, conf in zip(results.boxes.xyxy, results.boxes.cls, results.boxes.conf):
             x1, y1, x2, y2 = box
@@ -46,14 +46,14 @@ def detect_fire(fire):
             if class_name == 'fire':
                 fire = 1
                 img_optica = frame.copy()  # Guardamos la imagen original en el momento de detección
-                '''print(f"🔥 Incendio detectado - Centroide: ({cx}, {cy}) - Confianza: {conf:.2f}")
+                print(f"🔥 Incendio detectado - Centroide: ({cx}, {cy}) - Confianza: {conf:.2f}")
 
                 cv2.circle(annotated_frame, (cx, cy), 5, (0, 0, 255), -1)
                 label = f"{class_name} ({conf:.2f})"
                 cv2.putText(annotated_frame, label, (cx + 10, cy),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-        #cv2.imshow("🔥 Detección de Incendio - Webcam", annotated_frame)
+        cv2.imshow("🔥 Detección de Incendio - Webcam", annotated_frame)
 
         # Permitir salir manualmente presionando 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -61,6 +61,6 @@ def detect_fire(fire):
             break
 
     cap.release()
-    cv2.destroyAllWindows()'''
+    cv2.destroyAllWindows()
 
     return img_optica,cx,cy,fire_coordinates
