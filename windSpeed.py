0850023a1c8c88b658bda0ae91b751f7257247b6
mgcarlos14 <mgcar_los@hotmail.com>
2025-05-29 16:20:11 -0600

    Creación de Script para visualización de velocidad del viento

---
+++ windSpeed.py
@@ -0,0 +1,33 @@
+import leafmap
+import geemap
+import ee
+import webbrowser
+ee.Authenticate()
+ee.Initialize(project='light-sunup-288723')
+
+url = "https://github.com/opengeos/datasets/releases/download/raster/wind_global.nc"
+filename = "wind_global.nc"
+leafmap.download_file(url, output=filename, overwrite=True)
+data = leafmap.read_netcdf(filename)
+print(data)
+
+tif = "wind_global.tif"
+leafmap.netcdf_to_tif(filename, tif, variables=["u_wind", "v_wind"], shift_lon=True)
+geojson = ("https://github.com/opengeos/leafmap/raw/master/examples/data/countries.geojson")
+
+m = leafmap.Map(layers_control=True)
+m.add_basemap("CartoDB.DarkMatter")
+m.add_velocity(
+    filename,
+    zonal_speed="u_wind",
+    meridional_speed="v_wind",
+    color_scale=[
+        "rgb(0,0,150)",
+        "rgb(0,150,0)",
+        "rgb(255,255,0)",
+        "rgb(255,165,0)",
+        "rgb(150,0,0)",
+    ],
+)
+m.to_html("windMap.html")
+webbrowser.open("windMap.html")
