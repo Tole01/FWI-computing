b72f2e21203452d1dedf4b944dada11d4e656286
JacoboT27 <jacobotc2703@gmail.com>
2025-06-03 12:26:09 -0600

    config

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ FOV_OPTICA_VERTICAL = 140.4    # en grados
 FOV_TERMICA_HORIZONTAL = 95  # en grados
 FOV_TERMICA_VERTICAL = 71    # en grados
 
+#temperatura
+TEMP_MIN = -10  # Temperatura mínima en grados Celsius
+TEMP_MAX = 140   # Temperatura máxima en grados Celsius
+
 # Resoluciones de imagen
 THERMAL_WIDTH = 160
 THERMAL_HEIGHT = 120
